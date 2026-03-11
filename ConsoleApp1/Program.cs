2e6253abd9294339f09bdb19e277139b3feac41a
Rafael Pasquarella <rafaelpasquarella@gmail.com>
2026-03-10 22:29:58 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using CornwallUtilities.commands;
+using CornwallUtilities.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornwallUtilities
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.Guilds | DiscordIntents.GuildMessages | DiscordIntents.MessageContents,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+
+            Client.Ready += Client_Ready;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+              StringPrefixes = new string[] { jsonReader.prefix},
+              EnableMentionPrefix = true,
+              EnableDms = true,
+              EnableDefaultHelp = false,  
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<UtilityCommands>();
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs e)
+        {
+            Console.WriteLine("Bot is ready!");
+            return Task.CompletedTask;
+        }
+
+    }
+}
+
---
+++ ConsoleApp1/commands/utilitycommands.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.VisualBasic;
+
+namespace CornwallUtilities.commands
+{
+    internal class UtilityCommands : BaseCommandModule
+    {
+        public string userTag = "<@1072212634201505952>";
+        [Command("vsfdliliane")]
+        public async Task Vsfdliliane(CommandContext ctx)
+        {
+            const ulong allowedUserId = 1402344358199689348;
+            if (ctx.User.Id != allowedUserId)
+            {
+                await ctx.Channel.SendMessageAsync(new DiscordMessageBuilder()
+                .WithContent("Você não tem permissão para usar este comando.")
+                .WithReply(ctx.Message.Id));
+                return;
+            }
+            var responses = new[]
+            {
+                $"Vai se fuder {userTag}",
+                $"Vai tomar no cu,{userTag} porra",
+                $"Caralho, {userTag} burrão hein.",
+                $"Vai pra casa do chapéu, {userTag} fdppppp!!!!1!!1.",
+            };
+
+            var rand = new Random();
+            var response = responses[rand.Next(responses.Length)];
+            await ctx.Channel.SendMessageAsync(new DiscordMessageBuilder()
+                .WithContent(response)
+                .WithReply(ctx.Message.Id));
+        }
+    }
+}
